5816913642c6ca4416d7cfa51270688a7cc74be7
seciltuncay <lseciltuncay@gmail.com>
2022-06-23 17:09:18 +0300

    weird

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 const Loading = () => {
-	return (
-		<div className="container">
-			<div className="row loading-row justify-content-center align-items-center">
-				<div class="loading">
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-					<div class="dot"></div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="row loading-row justify-content-center align-items-center">
+        <div className="loading">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
--- src/components/TrendingMovies/TrendingMovies.js
+++ src/components/TrendingMovies/TrendingMovies.js
@@ -1,78 +1,78 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-	getTrendingMovies,
-	getFavoriteMovies,
-	getWatchList
+  getTrendingMovies,
+  getFavoriteMovies,
+  getWatchList,
 } from "../../features/movies/movieSlice";
 import { fetchAsyncTrendingMovies } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import ReactPaginate from "react-paginate";
 
 const TrendingMovies = () => {
-	const trendingMovies = useSelector(getTrendingMovies);
-	const favMovies = useSelector(getFavoriteMovies);
-	const watchListMovies = useSelector(getWatchList);
-	const dispatch = useDispatch();
-	const [currentPage, setCurrentPage] = useState(1);
-	const totalPages = trendingMovies.total_pages;
+  const trendingMovies = useSelector(getTrendingMovies);
+  const favMovies = useSelector(getFavoriteMovies);
+  const watchListMovies = useSelector(getWatchList);
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = trendingMovies.total_pages;
 
-	useEffect(() => {
-		dispatch(fetchAsyncTrendingMovies(currentPage));
-	}, [dispatch, currentPage]);
+  useEffect(() => {
+    dispatch(fetchAsyncTrendingMovies(currentPage));
+  }, [dispatch, currentPage]);
 
-	const handlePageClick = data => {
-		setCurrentPage(data.selected + 1);
-	};
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected + 1);
+  };
 
-	const isFavorite = movieID => {
-		return favMovies.some(movie => movie.id === movieID);
-	};
+  const isFavorite = (movieID) => {
+    return favMovies.some((movie) => movie.id === movieID);
+  };
 
-	const isWatchList = movieID => {
-		return watchListMovies.some(movie => movie.id === movieID);
-	};
+  const isWatchList = (movieID) => {
+    return watchListMovies.some((movie) => movie.id === movieID);
+  };
 
-	return (
+  return (
+    <div className="container">
+      <h1 className="my-4 text-center">Trending Movies</h1>
 
-			<div className="container">
-				<h1 className="my-4 text-center">Trending Movies</h1>
-				
-				<div className="row">
-					{trendingMovies.results &&
-						trendingMovies.results.map((movie, index) => (
-							<MovieCard
-								key={index}
-								movieData={movie}
-								isFavorite={isFavorite(movie.id)}
-								isWatchList={isWatchList(movie.id)}
-							/>
-						))}
-				</div>
-				
-	 				<ReactPaginate
-						previousLabel={"previous"}
-						nextLabel={"next"}
-						breakLabel={"..."}
-						pageCount={totalPages}
-						marginPagesDisplayed={4}
-						pageRangeDisplayed={4}
-						onPageChange={handlePageClick}
-						containerClassName={"pagination mt-3 justify-content-center movie-pagination"}
-						pageClassName={"page-item"}
-						pageLinkClassName={"page-link"}
-						previousClassName={"page-item"}
-						previousLinkClassName={"page-link"}
-						nextClassName={"page-item"}
-						nextLinkClassName={"page-link"}
-						breakClassName={"page-item"}
-						breakLinkClassName={"page-link"}
-						activeClassName={"active"}
-					/> 
-				
-			</div>
-		
-	);
+      <div className="row">
+        {trendingMovies.results &&
+          trendingMovies.results.map((movie, index) => (
+            <MovieCard
+              key={index}
+              movieData={movie}
+              isFavorite={isFavorite(movie.id)}
+              isWatchList={isWatchList(movie.id)}
+            />
+          ))}
+      </div>
+      <div className="row">
+        <ReactPaginate
+          previousLabel={"previous"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          pageCount={totalPages}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={2}
+          onPageChange={handlePageClick}
+          containerClassName={
+            "col-md-12 pagination mt-3 justify-content-center movie-pagination flex-wrap"
+          }
+          pageClassName={"page-item"}
+          pageLinkClassName={"page-link"}
+          previousClassName={"page-item"}
+          previousLinkClassName={"page-link"}
+          nextClassName={"page-item"}
+          nextLinkClassName={"page-link"}
+          breakClassName={"page-item"}
+          breakLinkClassName={"page-link"}
+          activeClassName={"active"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TrendingMovies;
